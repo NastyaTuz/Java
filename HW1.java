b0fbd5abc27e3e9babf1d9724d289e40cd792089
NastyaTuz <62496991+NastyaTuz@users.noreply.github.com>
2023-06-22 19:50:29 +0200

    Add files via upload

---
+++ HW1.java
@@ -0,0 +1,27 @@
+package org.hw1;
+
+public class HW1 {
+    public static void main(String[] args) {
+
+     char myChar = 'G';
+     int myInt = 89;
+     byte myByte = 4;
+     short myShort = 56;
+
+     float myFloat = 4.7333436F;
+     double myDouble = 4.355453532;
+
+     long myLong = 12121;
+
+        System.out.println("myChar="+myChar);
+        System.out.println("myInt="+myInt);
+        System.out.println("myByte="+myByte);
+        System.out.println("myShort="+myShort);
+
+        System.out.println("myFloat="+myFloat);
+        System.out.println("myDouble="+myDouble);
+
+        System.out.println("myLong="+myLong);
+
+    }
+}
